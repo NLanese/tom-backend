3a64f376eeb7c3b8c7f4362b035c9a50380994fd
Dylan Drechsel <drechseldylan@gmail.com>
2021-12-08 15:06:23 -0500

    completed Accident model

--- backend/graphql/typeDefs.js
+++ backend/graphql/typeDefs.js
@@ -5,6 +5,33 @@ const typeDefs = gql`
 	scalar Date
 	scalar JSON
 
+    type User {
+        id:                     ID
+        role:                   String
+        firstname:              String
+        lastname:               String
+        email:                  String
+        password:               String
+        fico:                   Int
+        netradyne:              Int
+        da:                     Int
+        seatbelt:               Boolean
+        speeding:               Boolean
+        defects:                Int
+        cdf:                    Int
+        dar:                    Int
+        dcr:                    Int
+        pod:                    Int
+        cc:                     Int
+        sc:                     Int
+        has_many_accidents:     Int
+        belongs_to_team:        Boolean
+        attendence:             JSON
+        productivity:           JSON
+
+        accidents:              [Accidents]
+    }
+
 `;
 
 export default typeDefs;
