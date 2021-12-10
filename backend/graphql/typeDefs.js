86ce6277f93d63ca99376775fbf419dba45993bb
DylanDrechsel <DylanDrechsel@gmail.com>
2021-12-10 14:18:34 -0500

    Tested updateAccident Mutation

--- backend/graphql/typeDefs.js
+++ backend/graphql/typeDefs.js
@@ -175,7 +175,7 @@ const typeDefs = gql`
 
     # ACCIDENT MUTATIONS
     createAccident(using_safety: Boolean, safety_failed: Boolean, number_package_carried: Int, safety_equipment_used: JSON, failed_safety: Boolean): Accident
-    updateAccident(using_safety: Boolean, safety_failed: Boolean, number_package_carried: Int, safety_equipment_used: JSON, failed_safety: Boolean): Accident
+    updateAccident(accidentId: Int, using_safety: Boolean, safety_failed: Boolean, number_package_carried: Int, safety_equipment_used: JSON, failed_safety: Boolean): Accident
     }
 
     #----------------------------------------END QUERIES AND MUTATIONS ----------------------------
