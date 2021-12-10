2b82804206dfccd50545892024d2fa31d76866e9
nlanese21 <nlanese21@gmail.com>
2021-12-10 09:50:58 -0500

    Completed Schema translation to typeDefs.js

--- backend/graphql/typeDefs.js
+++ backend/graphql/typeDefs.js
@@ -40,7 +40,120 @@ const typeDefs = gql`
         number_package_carried: Int
         safety_equipment_used:  JSON
         failed_safety:          Boolean
+
+        hitPerson:              [HitPerson]
+        thirdParty:             [thirdParty]
+        injuryAccident:         [InjuryAccident]
+        propertyAccident:       [PropertyAccident]
+        injuryReport:           [InjuryReport]
     }
+
+
+    type HitPerson{
+        id:                     ID
+        accidentID:             Int
+        accident:               [Accident]
+        medical_attention:      Boolean
+        vehicle_or_pedestrian:  String
+        previous_damage:        String
+        contact_information:    JSON
+        injury:                 String
+
+        accident_pictures:      [Image]
+    }
+
+
+    type ThirdParty{
+        id:                     ID
+        accidentId:             Int
+        accident:               [Accident]
+        location:               String
+    }
+
+
+    type InjuryAccident{
+        id:                     ID
+        self_injured:           Boolean
+        vehicle_number:         String
+        amazon_logo:            Boolean
+        exact_address:          String
+        action_before_accident: Json
+        police_report:          Json
+        weather:                String
+        wet_ground:             Boolean
+        slippery_ground:        Boolean
+        extra_info:             String
+        rushed_prior:           Boolean
+        
+        accident_pictures:      [Image]
+
+        accidentId:             Int
+        accident:               [Accident]
+    }
+
+
+    type PropertyAccident{
+      id:                       ID
+      self_injured:             Boolean
+      vehicle_number:           String
+      amazon_logo:              Boolean
+      exact_address:            String
+      action_before_accident:   Json
+      police_report:            Json
+      weather:                  String
+      wet_ground:               Boolean
+      slippery_ground:          Boolean
+      extra_info:               String
+      rushed_prior:             Boolean
+
+      accident_pictures:        [Image]
+
+      accidentId                Int
+      accident                  [Accident]
+    }
+
+
+    type InjuryReport{
+      id:                     ID
+      immediate_attentio:     Boolean
+      late:                   Json
+      self_injured:           Boolean
+      injury_type:            Json
+      other_injured:          Boolean
+      before_injury:          String
+      packages:               Json
+      safety_equipment:       Json
+      unsafe_conditions:      Json
+      pain_level:             Int
+      addtional_information:  String
+
+      accidentID:             Int
+      accident:               [Accident]
+    }
+
+
+
+    type Image{
+      id:                     ID
+      fieldname:              String
+      path:                   String
+      mimetype:               String
+      size:                   Int
+      originalname:           String
+      encoding:               String
+      destination:            String
+      filename:               String
+
+      injuryAccidentId:       Int
+      injuryAccident:         [InjuryAccident]
+
+      hitPersonId:            Int
+      hitPerson               [HitPerson]
+
+      propertyAccidentId:     Int
+      propertyAccident:       [PropertyAccident]       
+    }
+
 
     # ---------------------------------------- END SCHEMAS ----------------------------------------
 
