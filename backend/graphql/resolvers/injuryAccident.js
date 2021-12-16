c46e0626548f1c571804f990d2a7e106a3e25e9b
DylanDrechsel <DylanDrechsel@gmail.com>
2021-12-16 09:30:50 -0500

    tested handles functions from yesterday

--- backend/graphql/resolvers/injuryAccident.js
+++ backend/graphql/resolvers/injuryAccident.js
@@ -107,11 +107,64 @@ export default {
             }
         },
 
+        adminUpdateInjuryAccident: async (_, {
+            injuryAccidentId,
+            self_injured,
+            vehicle_number,
+            amazon_logo,
+            exact_address,
+            action_before_accident,
+            police_report,
+            weather,
+            wet_ground,
+            slippery_ground,
+            extra_info,
+            rushed_prior
+        }, context) => {
+            const admin = await checkAdminAuth(context)
+
+            const injuryAccident = await db.injuryAccident.findUnique({
+                where: {
+                    id: injuryAccidentId
+                }
+            })
+
+            if (!injuryAccident) {
+                throw new Error('Error: Injury accident record does not exist')
+            }
+
+            const verified = await handleAdminInjuryAccidentOwnership(admin.id, injuryAccidentId)
+
+            try {
+                if (verified) {
+                    return await db.injuryAccident.update({
+                        where: {
+                            id: injuryAccidentId
+                        },
+                        data: {
+                            self_injured: self_injured,
+                            vehicle_number: vehicle_number,
+                            amazon_logo: amazon_logo,
+                            exact_address: exact_address,
+                            action_before_accident: action_before_accident,
+                            police_report: police_report,
+                            weather: weather,
+                            wet_ground: wet_ground,
+                            slippery_ground: slippery_ground,
+                            extra_info: extra_info,
+                            rushed_prior: rushed_prior
+                        }
+                    })
+                }
+            } catch (error) {
+                throw new Error(error)
+            }
+        },
+
         deleteInjuryAccident: async (_, {
             injuryAccidentId
         }, context) => {
             const admin = await checkAdminAuth(context)
-            const verified = await handleAdminInjuryAccidentOwnership(admin.id, injuryAccidentId)
             const injuryAccident = await db.injuryAccident.findUnique({
                 where: {
                     id: injuryAccidentId
@@ -120,6 +173,7 @@ export default {
             if (!injuryAccident) {
                 throw new Error('Error: Injury accident record does not exist')
             }
+            const verified = await handleAdminInjuryAccidentOwnership(admin.id, injuryAccidentId)
             try {
                 if (verified) {
                     console.log("Delete in progress")
