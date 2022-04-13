66648b2464ec2d2f95d94170d7bdb93b7a79391e
NLanese <NLanese21@gmail.com>
2022-04-13 16:44:23 -0400

    added CORs line back

--- server.js
+++ server.js
@@ -47,11 +47,11 @@ const startApolloServer = async () => {
   ];
 
 
-  // app.use(function(req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "thetomapp.com"); // update to match the domain you will make the request from
-  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //   next();
-  // });
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "thetomapp.com"); // update to match the domain you will make the request from
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
   app.use(
     cors({
