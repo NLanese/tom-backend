f6036caa10d466dba5ccb4d96469e6f5683d9fdd
NLanese <NLanese21@gmail.com>
2022-05-27 12:32:23 -0400

    added owner and manager refetch mutations

---
+++ graphql/resolvers/manager/mutations/refreshManager.js
@@ -0,0 +1,52 @@
+import checkManagerAuth from "../../../../utils/checkAuthorization/check-manager-auth.js";
+import db from "../../../../utils/generatePrisma.js";
+export default {
+    Mutation: {
+        managerSignUp: async (_, { role, token} ) => {
+            let manager = checkManagerAuth(token)
+            try {
+                return await db.manager.findUnique({
+                    where: {
+                        id:  manager.id
+                    },
+                    include: {
+                        owner: true,
+                        drivers: {
+                            include: {
+                                weeklyReport: true,
+                                accidents: {
+                                    include: {
+                                        propertyAccidents: true,
+                                        collisionAccidents: true,
+                                        injuryAccidents: true
+                                    }
+                                }
+                            }
+                        },
+                        dsp: {
+                            include: {
+                                shifts: {
+                                    orderBy: {
+                                        date: 'asc'
+                                    }
+                                },
+                                devices: true
+                            }
+                        },
+                        messages: true,
+                        chatrooms: {
+                            include: {
+                                owner: true,
+                                managers: true,
+                                drivers: true,
+                                messages: true
+                            }
+                        }
+                    }
+                })
+            } catch (error) {
+                throw new Error(error)
+            }
+        }
+    }
+}
